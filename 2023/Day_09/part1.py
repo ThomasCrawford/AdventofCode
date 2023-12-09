bc643ed4c2e66685bbf50d1720c994870472916e
unknown <tomc1390@gmail.com>
2023-12-09 11:34:12 -0800

    Day 9

---
+++ 2023/Day_09/part1.py
@@ -0,0 +1,20 @@
+
+
+def main():
+	answer = 0
+	with open("input.txt") as file:
+		for line in file:
+			sequence = [int(x) for x in line.split()]
+			answer += predict(sequence)
+	print(answer)
+
+def predict(sequence):
+	#all 0
+	if all(x==0 for x in sequence):
+		return 0
+	else:
+		sub_sequence = [sequence[i+1] - sequence[i] for i in range(len(sequence)-1)]
+		return sequence[-1] + predict(sub_sequence)
+
+if __name__ == '__main__':
+	main()
