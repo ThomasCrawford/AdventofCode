936b39280d37f7226cf994a5c5437607aed2428c
unknown <tomc1390@gmail.com>
2024-07-14 18:18:44 -0700

    more progress on 17

--- 2018/day_17.py
+++ 2018/day_17.py
@@ -1,5 +1,7 @@
 import re
+import sys
 
+sys.setrecursionlimit(20000)
 
 
 def look_down(x, y, horr,vert):
@@ -17,7 +19,6 @@ def look_right(x, y, horr,vert):
 def look_left(x, y, horr,vert):
     for line in vert[::-1]:
         if line[0] < x and line[1]<= y <=line[2]:
-            print(line[0]+1)
             return line[0]
     return False
 
@@ -44,22 +45,69 @@ def look_left(x, y, horr,vert):
 #                     fill_count = (right-left)*(y-h)
 #                     return x, h, horr, fill_count
 
+
+def condense(h, horr):
+    for h2 in horr:
+        if h[0] == h2[0]:
+            if h[2] == h2[1]:
+                horr.remove(h2)
+                horr.append([h2[0], h[1], h2[2]])
+                return horr
+            if h[1] == h2[2]:
+                horr.remove(h2)
+                horr.append([h2[0], h2[1], h[2]])
+                return horr
+    horr.append(h)
+    return horr
+
+
+def display(horr, vert):
+
+    xmin = min([line[0] for line in vert])
+    xmax = max([line[0] for line in vert])
+
+    data = set()
+    for line in vert:
+        for y in range(line[1],line[2]+1):
+            data.add((line[0],y))
+    for line in horr:
+        for x in range(line[1],line[2]+1):
+            data.add((x, line[0]))
+
+
+    for y in range(crit_heights[-1]+1):
+    # for y in range(80):
+        out = ""
+        for x in range(xmin-1, xmax+2):
+            if x == 500 and y == 0:
+                out += "+"
+            elif (x,y) in data:
+                out += "#"
+            else:
+                out += "."
+        out += str(y)
+        print(out)
+
+
+
 def fill(x,y,horr,vert):
+    # print(f'filling {x}, {y}')
     left = look_left(x,y,horr,vert)
     right = look_right(x,y,horr,vert)
     for h in crit_heights[::-1]:
         if h < y:
             look_l = look_left(x, h, horr,vert)
             look_r = look_right(x, h, horr,vert)
+            # print(f'h {h}, left {left}, right {right}, look_l {look_l}, look_r {look_r}')
             #if container being filled has something in it
             if look_l > left or look_r < right:
-                horr.append([h+1,left,right])
+                horr = condense([h,left,right],horr)
                 horr.sort()
                 fill_count = (right-left)*(y-h-1) 
                 return horr, fill_count
             # if container overflows:
             if not look_l or not look_r or look_l < left or look_r > right:
-                horr.append([h + 1,left,right])
+                horr = condense([h + 1,left,right], horr)
                 horr.sort()
                 fill_count = (right-left)*(y-h)
                 return horr, fill_count
@@ -109,17 +157,15 @@ def fill_all(horr, vert):
     fill_count = 0
     while pools(x,y,horr,vert):
         x_to_fill, y_to_fill = pools(x,y,horr,vert)
-        print(f'Filling at {x_to_fill, y_to_fill}, horr = {horr}')
         horr, count = fill(x_to_fill, y_to_fill, horr, vert)
-        print(f'New horr: {horr}')
-        input()
         fill_count += count
+        display(horr,vert)
     return horr, fill_count
 
 
 vert = []
 horr = []
-with open("test.txt") as file:
+with open("input_17.txt") as file:
     for line in file:
         nums = [int(x) for x in re.findall(r'(\d+)', line)]
         if line[0] == "x":
@@ -136,47 +182,11 @@ c_heights= []
 for x in [line[1] for line in vert] + [line[2] for line in vert]:
     c_heights.append(x)
     c_heights.append(x-1)
-
 crit_heights = list(set(sorted(c_heights)))
-# print(len(crit_heights))
-
+  
 
 
 horr, count = fill_all(horr, vert)
 
+display(vert, horr)
 
-
-#Display
-xmin = min([line[0] for line in vert])
-xmax = max([line[0] for line in vert])
-
-data = set()
-for line in vert:
-    for y in range(line[1],line[2]+1):
-        data.add((line[0],y))
-for line in horr:
-    for x in range(line[1],line[2]+1):
-        data.add((x, line[0]))
-
-
-
-
-
-
-
-
-
-
-
-
-for y in range(crit_heights[-1]+1):
-    out = ""
-    for x in range(xmin-1, xmax+2):
-        if x == 500 and y == 0:
-            out += "+"
-        elif (x,y) in data:
-            out += "#"
-        else:
-            out += "."
-    out += str(y)
-    print(out)
